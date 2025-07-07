3802fef4ab18853eb56056a5a56d560689b98401
Ahsan Khan <62301158+Ahsan-khan7@users.noreply.github.com>
2025-07-07 13:05:29 +0530

    Update sample_test.py

--- backend/sample_test.py
+++ backend/sample_test.py
@@ -15,7 +15,7 @@ def is_prime(number):
 # Take input from user
 try:
     #num = int(input("Enter a number to check if it's prime: "))
-    num=0
+    num=7
     
     if is_prime(num):
         print(f"{num} is a prime number.")
